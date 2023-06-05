b777402fd8f7a6ce2bf1c063abbeac553ef8b217
Rodrigo Rojas Chaves <66186351+lexRojas@users.noreply.github.com>
2023-06-04 20:06:27 -0600

    Update database.php
    link db clearDB

--- php/database.php
+++ php/database.php
@@ -1,5 +1,5 @@
 <?php
 
-$db = mysqli_connect( 'localhost', 'basedatos','basedatos','notario');
+$db = mysqli_connect( 'us-cdbr-east-06.cleardb.net', 'bbe532e4e582c2','13228bd4','heroku_c9945836774c401');
 
 ?>
