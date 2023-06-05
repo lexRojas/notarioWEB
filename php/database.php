71506f6a58909b2616fb389348e835d4d0d48f55
Rodrigo Rojas Chaves <66186351+lexRojas@users.noreply.github.com>
2023-06-04 21:06:06 -0600

    v4

--- php/database.php
+++ php/database.php
@@ -1,7 +1,17 @@
 <?php
 
 $db = mysqli_init();
-$db -> ssl_set('bbe532e4e582c2-key.pem', 'bbe532e4e582c2-cert.pem','cleardb-ca.pem', null, null);
-$db -> real_connect('us-cdbr-east-06.cleardb.net', 'bbe532e4e582c2','13228bd4','heroku_c9945836774c401');
+mysqli_options ($db, MYSQLI_OPT_SSL_VERIFY_SERVER_CERT, true);
+
+$db->ssl_set('bbe532e4e582c2-key.pem', 'bbe532e4e582c2-cert.pem','cleardb-ca.pem', null, null);
+$link = mysqli_real_connect ($db, 'us-cdbr-east-06.cleardb.net', 'bbe532e4e582c2','13228bd4','heroku_c9945836774c401', 3306, NULL, MYSQLI_CLIENT_SSL);
+if (!$link)
+{
+    die ('Connect error (' . mysqli_connect_errno() . '): ' . mysqli_connect_error() . "\n");
+} else {
+    $res = $db->query('SHOW TABLES;');
+    print_r ($res);
+    $db->close();
+}
 
 ?>
