d659bd0c6c2e679186752a4f4edeeddfedf1d3be
Rodrigo Rojas Chaves <66186351+lexRojas@users.noreply.github.com>
2023-06-05 13:15:27 -0600

    Cambios
    1

--- php/database_aws.php
+++ php/database_aws.php
@@ -1,10 +1,15 @@
 <?php
 // Create connection
+/*
+$hostname = 'localhost';
+$username = 'id20870862_basedatos'; 
+$password = 'C0c0l0c0++';
+$database = 'id20870862_notario'; 
+*/
 $hostname = 'jtb9ia3h1pgevwb1.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
 $username = 'vl0v51r0t6ald9dh'; 
 $password = 'nq5d3dq46cc551cj';
 $database = 'u67qxne0bs0uujx8'; 
-
 
 $db = mysqli_connect($hostname, $username, $password, $database);
 
--- php/get_registros.php
+++ php/get_registros.php
@@ -2,7 +2,7 @@
 
   include 'database_aws.php'; 
 
-  $query = 'SELECT registro.id_registro as id, registro.registro_descripcion as valor FROM u67qxne0bs0uujx8.registro;';
+  $query = 'SELECT registro.id_registro as id, registro.registro_descripcion as valor FROM registro;';
 
   $result  = mysqli_query($db,$query);
 
