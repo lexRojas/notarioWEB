c2c4ad9f04b1b0c6e1578f30fe06ca4e45480470
Rodrigo Rojas Chaves <66186351+lexRojas@users.noreply.github.com>
2023-06-04 21:01:43 -0600

    v3

--- php/get_registros.php
+++ php/get_registros.php
@@ -1,14 +1,25 @@
 
 <?php 
 
-  require 'database.php';
+  include 'php\database.php'; 
 
-  $query = "select id_registro as id,registro_descripcion as valor from registro";
+  $query = "SELECT registro.id_registro as id, registro.registro_descripcion as valor FROM heroku_c9945836774c401.registro;";
 
   $result  = mysqli_query($db,$query);
 
+echo 'buscando registros...';
+
+
   if(!$result) {
+
+      echo 'algo fallo!!!';
+      echo '<pre>';
+      echo $result;
+      echo '</pre>';       
       die('Query Failed'. mysqli_error($db));
+  }else{
+    
+
   }
   
   $json = array();
